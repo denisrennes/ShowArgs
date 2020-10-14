15d61dd0041e54b696c6af7337e147be92f44ff8
denisrennes <degaupub@gmail.com>
2020-10-14 21:29:58 +0200

    Added a final pause "Press any key..."

--- ShowArgs/ShowArgs.cpp
+++ ShowArgs/ShowArgs.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	cout << "ShowArgs.exe 1.0 outputs its command line arguments:" << endl;
+	cout << "ShowArgs.exe 1.1 outputs its command line arguments:" << endl;
 	cout << "   \"argc: nb_args\" is the number of arguments. nb_args is at least 1 because the program name is passed by the operating system as the 1st argument." << endl;
 	cout << "   \"argv[x]: >arg_value<\" are the values of the arguments. Surrounding characters > < are delimiters, not part of the value." << endl;
 	cout << endl;
@@ -15,5 +15,5 @@ int main(int argc, char *argv[])
 	for (int i = 0; i < argc; i++) {
 		cout << "argv[" << i << "]: >" << argv[i] << "<" << endl;
 	}
-
+	system("pause");
 }
